45e96a265cfb30e112536fafac1e82fde730bc03
RajapndiR <rajapndi.r@quadkast.co.in>
2024-10-17 21:29:59 +0530

    task apis completed

--- src/controllers/user/authRouter.ts
+++ src/controllers/user/authRouter.ts
@@ -2,7 +2,7 @@ import { express } from "../../helpers/path";
 import UserController from "./controller"
 const app = express.Router();
 
-app.route("/").post(UserController.createUser)
+app.route("/signup").post(UserController.createUser)
 app.route("/login").post(UserController.loginUser)
 
 export const LoginUserRouter = app;
